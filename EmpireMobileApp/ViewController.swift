9de1bcf074c0263e09e7fe0ae909fa3314c240d0
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2019-10-12 18:07:31 +0200

    Initial Commit

---
+++ EmpireMobileApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmpireMobileApp
+//
+//  Created by Rodrigo Nunes on 10/12/19.
+//  Copyright © 2019 Rodrigo Nunes Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
