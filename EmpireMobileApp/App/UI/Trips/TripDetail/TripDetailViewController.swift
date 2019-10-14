63a141256bca466a8dee0bdea8c98cf806a9c2e8
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2019-10-14 09:58:31 +0200

    Create TripDetail module

--- EmpireMobileApp/App/UI/Trips/TripDetail/TripDetailViewController.swift
+++ EmpireMobileApp/App/UI/Trips/TripDetail/TripDetailViewController.swift
@@ -7,11 +7,122 @@
 //
 
 import UIKit
+import RxSwift
+import Kingfisher
 
 class TripDetailViewController: UIViewController {
-
+    @IBOutlet private var pickUpPlanetImageView: UIImageView!
+    @IBOutlet private var dropOffPlanetImageView: UIImageView!
+    @IBOutlet private var tripInformationView: UIView!
+    @IBOutlet private var pilotImageView: UIImageView!
+    @IBOutlet private var pilotNameLabel: UILabel!
+    @IBOutlet private var departureLocationLabel: UILabel!
+    @IBOutlet private var departureTimeLabel: UILabel!
+    @IBOutlet private var arrivalLocationLabel: UILabel!
+    @IBOutlet private var arrivalTimeLabel: UILabel!
+    @IBOutlet private var tripDistanceLabel: UILabel!
+    @IBOutlet private var tripDurationLabel: UILabel!
+    @IBOutlet private var ratingView: RatingView!
+    
+    private let viewModel: TripDetailViewModelApi
+    private let disposeBag = DisposeBag()
+    
+    init(viewModel: TripDetailViewModelApi) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationController()
+        setupTripInformationView()
+        setupRx()
+    }
+}
 
+private extension TripDetailViewController {
+    func setupNavigationController() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        self.navigationController?.navigationBar.tintColor = .white
+    }
+    
+    func setupTripInformationView() {
+        let blurEffect: UIBlurEffect
+        blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tripInformationView.addSubview(blurEffectView)
+        tripInformationView.sendSubviewToBack(blurEffectView)
+        tripInformationView.layer.cornerRadius = 15
+    }
+    
+    func setupRx() {
+        setupPickupPlanetRx()
+        setupDropOffPlanetRx()
+        setupPilotRx()
+        setupRatingRx()
+        setupTripRx()
+    }
+    
+    func setupPickupPlanetRx() {
+        viewModel.pickupPlanetName.subscribe(onNext: {[unowned self] planetName in
+            self.departureLocationLabel.text = planetName
+        }).disposed(by: disposeBag)
+        
+        viewModel.pickupPlanetTime.subscribe(onNext: {[unowned self] timeString in
+            self.departureTimeLabel.text = timeString
+        }).disposed(by: disposeBag)
+        
+        viewModel.pickupPlanetImageURL.subscribe(onNext: {[unowned self] url in
+            self.pickUpPlanetImageView.kf.setImage(with: url)
+        }).disposed(by: disposeBag)
+    }
+    
+    func setupDropOffPlanetRx() {
+        viewModel.dropOffPlanetName.subscribe(onNext: {[unowned self] planetName in
+            self.arrivalLocationLabel.text = planetName
+        }).disposed(by: disposeBag)
+        
+        viewModel.dropOffPlanetTime.subscribe(onNext: {[unowned self] timeString in
+            self.arrivalTimeLabel.text = timeString
+        }).disposed(by: disposeBag)
+        
+        viewModel.dropOffPlanetImageURL.subscribe(onNext: {[unowned self] url in
+            self.dropOffPlanetImageView.kf.setImage(with: url)
+        }).disposed(by: disposeBag)
+    }
+    
+    func setupPilotRx() {
+        viewModel.pilotName.subscribe(onNext: {[unowned self] pilotName in
+            self.pilotNameLabel.text = pilotName
+        }).disposed(by: disposeBag)
+        
+        viewModel.pilotAvatar.subscribe(onNext: {[unowned self] url in
+            self.pilotImageView.kf.setImage(with: url)
+        }).disposed(by: disposeBag)
+    }
+    
+    func setupRatingRx() {
+        viewModel.pilotRating.subscribe(onNext: {[unowned self] ratingViewModel in
+            self.ratingView.setup(viewModel: ratingViewModel)
+        }).disposed(by: disposeBag)
+    }
+    
+    func setupTripRx() {
+        viewModel.tripDistance.subscribe(onNext: {[unowned self] distance in
+            self.tripDistanceLabel.text = distance
+        }).disposed(by: disposeBag)
+        
+        viewModel.tripDuration.subscribe(onNext: {[unowned self] duration in
+            self.tripDurationLabel.text = duration
+        }).disposed(by: disposeBag)
     }
 }
--- EmpireMobileApp/App/UI/Trips/TripDetail/TripDetailViewModel.swift
+++ EmpireMobileApp/App/UI/Trips/TripDetail/TripDetailViewModel.swift
@@ -7,5 +7,131 @@
 //
 
 import Foundation
+import RxSwift
 
+protocol TripDetailViewModelApi {
+    var pilotName: Observable<String> {get}
+    var pilotAvatar: Observable<URL> { get }
+    var pickupPlanetImageURL: Observable<URL> { get }
+    var pickupPlanetName: Observable<String> { get }
+    var pickupPlanetTime: Observable<String> { get }
+    var dropOffPlanetImageURL: Observable<URL> { get }
+    var dropOffPlanetName: Observable<String> { get }
+    var dropOffPlanetTime: Observable<String> { get }
+    var tripDistance: Observable<String> { get }
+    var tripDuration: Observable<String> { get }
+    var pilotRating: Observable<RatingViewViewModel> { get }
+}
 
+class TripDetailViewModel {
+    let trip: Trip!
+    
+    init(trip: Trip) {
+        self.trip = trip
+    }
+}
+
+extension TripDetailViewModel: TripDetailViewModelApi {
+    
+    var pilotAvatar: Observable<URL> {
+        return Observable.just(trip.pilot.avatar).map { (urlString) -> URL in
+            guard let url = URL(string: urlString) else {
+                return URL(string: "https://images.ctfassets.net/bl73eiperqoo/5j48PEcu5AmGRewyTRL8uN/5ec4f14a8775f1701a31ed49de0dc2ff/OG_porg.png")!
+            }
+            return url
+        }
+    }
+    
+    var pilotName: Observable<String> {
+        return Observable.just(trip.pilot.name)
+    }
+    
+    var pickupPlanetImageURL: Observable<URL> {
+        return Observable.just(trip.pickUp.picture).map { (urlString) -> URL in
+            guard let url = URL(string: urlString) else {
+                return URL(string: "https://66.media.tumblr.com/358dfd0733668010d9cad0ac6c2c0578/tumblr_pc50ffy6zo1vs5f7yo1_1280.png")!
+            }
+            return url
+        }
+    }
+    
+    var pickupPlanetName: Observable<String> {
+        return Observable.just(trip.pickUp.name)
+    }
+    
+    var pickupPlanetTime: Observable<String> {
+        return Observable.just(trip.pickUp.date)
+            .map {[unowned self] (date) -> String in
+                self.transformDateToDateString(date: date)
+        }
+    }
+    
+    var dropOffPlanetImageURL: Observable<URL> {
+        return Observable.just(trip.dropOff.picture).map { (urlString) -> URL in
+            guard let url = URL(string: urlString) else {
+                return URL(string: "https://66.media.tumblr.com/358dfd0733668010d9cad0ac6c2c0578/tumblr_pc50ffy6zo1vs5f7yo1_1280.png")!
+            }
+            return url
+        }
+    }
+    
+    var dropOffPlanetName: Observable<String> {
+        return Observable.just(trip.dropOff.name)
+    }
+    
+    var dropOffPlanetTime: Observable<String> {
+        return Observable.just(trip.dropOff.date)
+            .map {[unowned self] (date) -> String in
+                self.transformDateToDateString(date: date)
+        }
+    }
+    
+    var tripDistance: Observable<String> {
+        return Observable.just(trip.distance)
+            .map {[unowned self] (distance) -> String in
+                self.formatDistance(distance: distance)
+        }
+    }
+    
+    var tripDuration: Observable<String> {
+        return Observable.just(trip.getTripDuration())
+            .map {[unowned self] dateInterval -> String in
+            self.transformTimeIntervalToString(dateInterval: dateInterval)
+        }
+    }
+    
+    var pilotRating: Observable<RatingViewViewModel> {
+        return Observable.just(trip.pilot.rating)
+            .map { rating -> RatingViewViewModel in
+            return RatingViewViewModel(rating: rating, style: .big)
+        }
+    }
+}
+
+private extension TripDetailViewModel {
+    func transformDateToDateString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "h:mm"
+        formatter.amSymbol = "AM"
+        formatter.pmSymbol = "PM"
+        return formatter.string(from: date)
+    }
+    
+    func transformTimeIntervalToString(dateInterval: DateInterval) -> String {
+        let time = Int(dateInterval.duration)
+        let seconds = time % 60
+        let minutes = (time / 60) % 60
+        let hours = time / 3600
+        return "\(hours):\(minutes):\(seconds)"
+    }
+    
+    func formatDistance(distance: Distance) -> String {
+        let formatter = NumberFormatter()
+        formatter.groupingSeparator = ","
+        formatter.numberStyle = .decimal
+        let distanceString = formatter.string(for: distance.value)!
+        let unit = distance.unit.uppercased()
+        return "\(distanceString) \(unit)"
+    }
+}
